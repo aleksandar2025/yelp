82d23f61914ac86d67dff7a8a948b5b912031987
Alex Ivas <goodguyfc@protonmail.com>
2018-12-06 12:02:18 +0000

    add mongolab

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var expressSanitizer      = require('express-sanitizer');
 
 
 mongoose.set("useFindAndModify", false);
-mongoose.connect('mongodb://localhost/yelp_campv12Deployed', { useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost/yelp_campv12Deployed', { useNewUrlParser: true });
+mongoose.connect("mongodb://Alex:password123@ds223063.mlab.com:23063/yelpcampv12deployed", { useNewUrlParser: true })
 mongoose.set('useCreateIndex', true);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
